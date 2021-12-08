10fbfb0a84cfbe7b4830eac390cd0878dcf061ff
evanina <eva.degano@gmail.com>
2021-12-08 11:17:41 +0100

    dashboard html and css

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 const settingsBtn = document.querySelector(".uil-setting");
 const newTaskInput = document.querySelector("#new-task-input");
 
+
 // toggle the settings menu
 settingsBtn.addEventListener("click", () => {
   const toggleLinks = document.querySelector("#toggle-links");
@@ -9,15 +10,5 @@ settingsBtn.addEventListener("click", () => {
     toggleLinks.style.display = "block";
   } else {
     toggleLinks.style.display = "none";
-  }
-})
-
-newTaskInput.addEventListener("click", () => {
-  const goalSelector = document.querySelector("#goal-select");
-  
-  if (goalSelector.style.display === "none") {
-    goalSelector.style.display = "block";
-  } else {
-    goalSelector.style.display = "none";
   }
 })
