bb02136fa5fdee37bb8372b3911a4f5ea50ee394
Emma HB <emmanuelle.bolognini@gmail.com>
2021-12-10 18:28:40 +0100

    Dashboard routes

--- routes/user.js
+++ routes/user.js
@@ -16,15 +16,15 @@ router.get("/dashboard", isLoggedIn, (req, res, next) => {
   // capitalize first letter of user's first name
   req.session.user.firstName = req.session.user.firstName[0].toUpperCase() + req.session.user.firstName.substring(1);
   // TODO: récupérer l'objet "zenQuote" grâce à l'API Zen Quote
-  const p1 = axios.get('https://zenquotes.io/api/today/')
+  const p1 = axios.get('https://zenquotes.io/api/today/');
   // TODO: chercher les objectifs du user dans la DB
   // TODO: filtre des objectifs : isDone = false  
-  const p2 = Goal.find({ isDone: false })
+  const p2 = Goal.find({ isDone: false });
   // TODO: chercher les tâches du user dans la DB
   // TODO: filtre des tâches : endDate = today, endDate < today and isDone = false  
-  const p3 = Task.find({ endDate: { $lte: Date.now() }, isDone: false })
+  const p3 = Task.find({ endDate: { $lte: Date.now() }, isDone: false });
   // TODO: filtre des tâches en retard: endDate > today and isDone = false  
-  const p4 = Task.find({ endDate: { $gt: Date.now() }, isDone: false })
+  const p4 = Task.find({ endDate: { $gt: Date.now() }, isDone: false });
 
   Promise.all([p1, p2, p3, p4])
     .then(function([response, goalsFromDb, tasksFromDb, overdueTasksFromDb]) {
@@ -42,6 +42,7 @@ router.get("/dashboard", isLoggedIn, (req, res, next) => {
 })
 
 // POST /dashboard
+//Goals routes
 router.post("/goals", isLoggedIn, (req, res, next) => {
   // TODO: permettre au user d'ajouter des objectifs dans la DB
   Goal.create({ 
@@ -53,9 +54,29 @@ router.post("/goals", isLoggedIn, (req, res, next) => {
     category: req.body.category
   })
     .then(newGoal => res.redirect('/user/dashboard'))
-    .catch((err) => res.render('/user/dashboard'))
+    .catch((err) => res.redirect('/user/dashboard'))
 })
 
+router.post("/goals/:id/edit", isLoggedIn, (req, res, next) => {
+  Goal.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
+    color: req.body.color,
+    //category: req.body.category
+  })
+    .then(goal => res.redirect('/user/dashboard'))
+    .catch((err) => res.redirect('/user/dashboard'))
+})
+
+router.post("/goals/:id/delete", isLoggedIn, (req, res, next) => {
+  Goal.findByIdAndRemove(req.params.id)
+  .then(() => res.redirect("/user/dashboard"))
+  .catch((err) => res.redirect('/user/dashboard'))
+})
+
+
+//Tasks routes
 router.post("/tasks", isLoggedIn, (req, res, next) => {
   // TODO: permettre au user d'ajouter des tâches dans la DB
   Task.create({ 
@@ -64,7 +85,24 @@ router.post("/tasks", isLoggedIn, (req, res, next) => {
     title: req.body.title
   })
     .then(newTask => res.redirect('/user/dashboard'))
-    .catch(err => res.render('/user/dashboard'))
+    .catch(err => res.redirect('/user/dashboard'))
+})
+
+router.post("/tasks/:id/edit", isLoggedIn, (req, res, next) => {
+  // TODO: permettre au user d'ajouter des tâches dans la DB
+  Task.findByIdAndUpdate(req.params.id, {
+    user_id: req.session.user._id,
+    goal_id: req.body.taskGoal,
+    title: req.body.title
+  })
+    .then(task => res.redirect('/user/dashboard'))
+    .catch(err => res.redirect('/user/dashboard'))
+})
+
+router.post("/tasks/:id/delete", isLoggedIn, (req, res, next) => {
+  Goal.findByIdAndRemove(req.params.id)
+  .then(() => res.redirect("/user/dashboard"))
+  .catch((err) => res.redirect('/user/dashboard'))
 })
 
 // GET /profile
