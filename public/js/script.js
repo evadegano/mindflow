44fc1f5b13149cf69ea0f1b98d47c522174bc81a
evanina <eva.degano@gmail.com>
2021-12-11 10:31:15 +0100

    dashboard html and css

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,20 @@
 const settingsBtn = document.querySelector(".uil-setting");
 const addInput = document.querySelector("#add-input");
 const collapseMenu = document.querySelectorAll(".collapsible-menu");
-const addGoalForm = document.querySelectorAll(".add-goal-form");
+const addGoalForms = document.querySelectorAll(".add-goal-form");
+const addGoalInputs = document.querySelectorAll(".add-goal-form .add-input");
 const addTaskInput = document.querySelector("#add-task-form .add-input");
+const addTaskOptions = document.querySelector("#add-task-form .options");
+const addTaskForm = document.querySelector("#add-task-form");
+const toggleMenu = document.querySelector("#toggle-menu");
+
+const pomodoroStartBtn = document.querySelector("#pomodoro--start-btn");
+const pomodoroMins = document.querySelector("#pomodoro--mins");
+const pomodoroSecs = document.querySelector("#pomodoro--secs");
+const pomodoroRing = document.querySelector("#pomodoro--progress-ring");
+const ringLength = pomodoroRing.getTotalLength();
+const duration = 10;
+const progress = ringLength / duration;
 
 window.addEventListener("load", () => {
   // get today's full date
@@ -17,14 +29,9 @@ window.addEventListener("load", () => {
 })
 
 // toggle the settings menu
+const toggleLinks = document.querySelector("#toggle-links");
 settingsBtn.addEventListener("click", () => {
-  const toggleLinks = document.querySelector("#toggle-links");
-
-  if (toggleLinks.style.display === "none") {
-    toggleLinks.style.display = "block";
-  } else {
-    toggleLinks.style.display = "none";
-  }
+  toggleLinks.classList.toggle('active');
 })
 
 // toggle dropdown menus
@@ -45,33 +52,39 @@ collapseMenu.forEach(menu => {
   })
 })
 
-// display form options on focus
+// display new task options on focus
 addTaskInput.addEventListener("focus", () => {
-  const addTaskOptions = document.querySelector("#add-task-form .options");
+  addTaskOptions.classList.add("active");
+  addTaskInput.classList.add("active");
+})
 
-  addTaskOptions.style.display = "block";
-  addTaskInput.style.borderBottom = "1px solid var(--light-grey)";
-  addTaskInput.style.paddingBottom = "10px";
+// display new goal options on focus
+addGoalForms.forEach((form) => {
+  form.querySelector(".add-input").addEventListener("focus", () => {
+    form.querySelector(".options").classList.add("active");
+  })
 })
 
 document.addEventListener("click", event => {
-  const addTaskForm = document.querySelector("#add-task-form");
-  const addTaskOptions = document.querySelector("#add-task-form .options");
-
+  // hide task options
   if (!addTaskForm.contains(event.target)) {
-    addTaskOptions.style.display = "none";
-    addTaskInput.style.borderBottom = "none";
-    addTaskInput.style.paddingBottom = "0";
+    addTaskOptions.classList.remove("active");
+    addTaskInput.classList.remove("active");
     addTaskInput.value = "";
   }
-})
 
-// hide form options on focus out
-//addTaskForm.addEventListener("focusout", () => {
-  //const addTaskForm = document.querySelector("#add-task-form");
-  //const addTaskOptions = document.querySelector("#add-task-form .options");
-  //addTaskOptions.style.display = "none";
-//})
+  // hide goal options
+  addGoalForms.forEach((form) => {
+    if (!form.contains(event.target)) {
+      form.querySelector(".options").classList.remove("active");
+    }
+  })
+
+  // hide settings menu
+  if (!toggleMenu.contains(event.target)) {
+    toggleLinks.classList.remove("active");
+  }
+})
 
 // change greeting message
 function switchGreetingMsg(hour) {
@@ -92,3 +105,37 @@ function switchGreetingMsg(hour) {
       break;
   }
 }
+
+// pomodoro timer
+pomodoroRing.style.strokeDasharray = ringLength + " " + ringLength;
+pomodoroRing.style.strokeDashoffset = ringLength;
+
+pomodoroStartBtn.addEventListener("click", () => {
+  let steps = 0;
+  let pomodoroTimer = setInterval(pomodoroProgress, 1000);
+  
+  function pomodoroProgress() {
+    if (steps === duration) {
+      clearInterval(pomodoroTimer);
+      pomodoroRing.style.strokeDashoffset = ringLength;
+      pomodoroMins.textContent = "00";
+      pomodoroSecs.textContent = "00";
+    } else {
+      pomodoroRing.style.strokeDashoffset -= progress;
+      steps++;
+      let mins = Math.floor(steps / 60);
+      let secs = steps % 60;
+
+      if (mins < 10) {
+        mins = "0" + mins;
+      }
+
+      if (secs < 10) {
+        secs = "0" + secs;
+      }
+
+      pomodoroMins.textContent = mins;
+      pomodoroSecs.textContent = secs;
+    }
+  }
+})
