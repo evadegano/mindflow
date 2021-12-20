7b39b3271cbed7028fc836887d318075ca070e97
evanina <eva.degano@gmail.com>
2021-12-20 14:55:10 +0100

    profile page design

--- routes/user.js
+++ routes/user.js
@@ -225,6 +225,9 @@ router.post("/tasks/:id/delete", isLoggedIn, (req, res, next) => {
 
 // GET /profile
 router.get("/profile", isLoggedIn, (req, res, next) => {
+  // capitalize first letter of user's first name
+  req.session.user.firstName = req.session.user.firstName[0].toUpperCase() + req.session.user.firstName.substring(1);
+  
   res.render("user/profile", {
     currentUser: req.session.user
   });
