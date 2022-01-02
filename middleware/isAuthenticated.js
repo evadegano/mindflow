c11b9e3bcf18cc477d3b627ce70f4961e7a0bd53
evanina <eva.degano@gmail.com>
2022-01-02 15:00:58 +0100

    homepage css

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,8 +1,7 @@
 module.exports = (req, res, next) => {
     // checks if the user is logged in when trying to access a specific page
-    if (req.isAuthenticated()) {
-      return next();
-    } else {
-      res.redirect('/auth/login');
-    }
+    if (!req.isAuthenticated()) {
+      return res.redirect('/auth/login');
+    } 
+    next();
 }
